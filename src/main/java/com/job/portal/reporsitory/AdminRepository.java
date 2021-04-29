22bde5b64dbbfb4552e32ed534d0a520fb177a2b
Abhijeet Das <pireats-abhi>
2021-04-29 18:45:36 +0600

    Added findByUserName method in AdminRepository

--- src/main/java/com/job/portal/reporsitory/AdminRepository.java
+++ src/main/java/com/job/portal/reporsitory/AdminRepository.java
@@ -3,5 +3,9 @@ package com.job.portal.reporsitory;
 import com.job.portal.entity.Admin;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends CrudRepository<Admin, Long> {
+
+    Optional<Admin> findByUserName(String userName);
 }
